76498013218273c168359a1eabc83f3d5f02c871
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2024-04-22 15:16:44 +0200

    Update ValuableTransporter.java

--- Assignment4/src/ValuableTransporter.java
+++ Assignment4/src/ValuableTransporter.java
@@ -30,9 +30,12 @@ public class ValuableTransporter implements Runnable
 
                 }
 
-                WriteTreasure treasure = door.acquireWrite();
+                WriteTreasure treasure = door.acquireWriteAccess();
                 for (int i = 0; i < 3; i++) {
-                    treasure.
+                    try {Thread.sleep(2000);}
+                    catch (InterruptedException e) {throw new RuntimeException(e);}
+                    treasure.addTreasure(valuables.remove(i));
+                    log.log(Thread.currentThread().getName()+ " delivered treasure ");
                 }
 
             }
