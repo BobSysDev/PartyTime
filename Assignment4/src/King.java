291c21901bd617155c092bb9a32c61e107149250
Electimore <61228588+Electimore@users.noreply.github.com>
2024-04-22 15:48:56 +0200

    Update King.java

--- Assignment4/src/King.java
+++ Assignment4/src/King.java
@@ -56,6 +56,9 @@ public class King implements Runnable
       }catch(InterruptedException e){
         e.printStackTrace();
       }
+
+      door.releaseWriteAccess();
+      lock = null;
     }
   }
 }
